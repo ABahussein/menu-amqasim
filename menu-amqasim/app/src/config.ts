db89a06366d9152a0e6e0cae7b4bfb3e12388f2b
ABahussein <abo.abdu.22@hotmail.com>
2025-11-16 00:15:16 +0300

    Update config.ts

--- menu-amqasim/app/src/config.ts
+++ menu-amqasim/app/src/config.ts
@@ -1,5 +1,5 @@
 export const MONGOURI =
-  "mongodb+srv://abdulrahman2:Aa0551412110#@cluster0.nqj0t73.mongodb.net/amqasim?appName=Cluster0";
+  "mongodb+srv://abdulrahman2:Aa0551412110%23@cluster0.nqj0t73.mongodb.net/amqasim?appName=Cluster0";
 export const JWT_SECRET = "UIY#$()*@#()&*(*JKLL34234#@JKL";
 export const JWT_EXPIRE = "60d";
 
